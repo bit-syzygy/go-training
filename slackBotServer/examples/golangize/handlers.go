67d51b982b5daceb7fcc595354565e4cdb991e54
None <none@noreply.com>
2017-07-21 20:49:06 +0100

    fixed case sensitivity of golangize

--- slackBotServer/examples/golangize/handlers.go
+++ slackBotServer/examples/golangize/handlers.go
@@ -12,7 +12,7 @@ type GoMatch struct{}
 
 //Match tests if the message had "go" in it
 func (g GoMatch) Match(msg *slack.MessageEvent) bool {
-	if strings.Contains(msg.Text, "go") {
+	if strings.Contains(msg.Text, "go") || strings.Contains(msg.Text, "Go") || strings.Contains(msg.Text, "GO") {
 		return true
 	}
 	return false
@@ -23,7 +23,11 @@ type GoHandle struct{}
 
 //Handle replaces go with golang
 func (h GoHandle) Handle(msg *slack.MessageEvent) string {
-	reply := fmt.Sprintf("Did you mean \"%s\" ?", strings.Replace(msg.Text, "go", "golang", -1))
+	text := msg.Text
+	text = strings.Replace(text, "go", "golang", -1)
+	text = strings.Replace(text, "Go", "Golang", -1)
+	text = strings.Replace(text, "GO", "GOLANG", -1)
+	reply := fmt.Sprintf("Did you mean \"%s\" ?", text)
 	fmt.Printf("received \"%s\" sending reply \"%s\" \n", msg.Text, reply)
 	return reply
 }
